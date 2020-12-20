804ec8d9fe77c3f368d7ace56ed8efc5666b9317
rsneh <me@ronsneh.com>
2020-12-20 16:04:38 +0200

    Update 404 page

--- pages/404.js
+++ pages/404.js
@@ -13,11 +13,11 @@ export default function Custom404() {
       <div id="layout" className="flex flex-col min-h-screen antialiased">
         <Header />
         <div className="flex flex-1 flex-col justify-center">
-          <div className="my-0 mx-auto block relative w-1/4">
+          <div className="my-0 mx-auto block relative md:w-1/4">
             <div className="opacity-75">
               <Page404Icon />
             </div>
-            <div className="mt-10 text-center">
+            <div className="mt-10 text-center w-2/3 mx-auto md:w-full">
               <h1 className="text-4xl font-bold">Not Found</h1>
               <p className="text-black my-2">It seem that we can't find the page you're looking for.</p>
               <div className="mt-10">
