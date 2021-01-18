be6b91b7ed8cc844f8bf51a59c6dbf2f0f964709
rsneh <me@ronsneh.com>
2021-01-18 16:20:21 +0200

    Fix publis API resolved without response

--- pages/api/post/[id]/publish.js
+++ pages/api/post/[id]/publish.js
@@ -1,16 +1,16 @@
 import { publishPostWithId } from 'utils/contentful'
 
-export default function postPublishHandler(req, res) {
+export default async function postPublishHandler(req, res) {
   const {
     query: { id },
   } = req
 
-  publishPostWithId(id)
-    .then(entry => {
-      res.status(200).json(entry)
-    })
-    .catch(err => {
-      console.log(err)
-      res.status(400).json({ message: 'Unable to publish post.' })
-    })
+  const entry = await publishPostWithId(id)
+  if (!entry) {
+    console.log(err)
+    res.status(400).json({ message: 'Unable to publish post.' })
+  }
+  else {
+    res.status(200).json(entry)
+  }
 }
